739cb1023d8b326daf76f199d796bc0178916d8a
Le Cao Tung Lam <Kevinle231299@gmail.com>
2022-06-08 14:36:13 +0700

    Graph interactive and new perturbation method
    - Change graph from static form to interactive from (long to HTML)
    - New method to quantify multiple perturbations effect to DFI and CFI

---
+++ Package/Step0_Graphs.R
@@ -0,0 +1,146 @@
+  ##################################################################
+  #               JRP-INRA data - STEP 0: Data treatment
+  #               Masoomeh and Hieu 04/12/2017
+  #
+  #       Plot the graphs of DFI and CFI
+  ##################################################################
+
+
+  rm(list=ls(all=TRUE)) #To remove the hisory
+  dev.off() #To close all graphs
+
+  #-------------------------------------------------------------------------------
+  # Packages needed for estimaton of Ideal trajectory - nonlinear regression
+  #-------------------------------------------------------------------------------
+
+  library(GrapheR)
+  library(lattice)
+  library(ggplot2)
+  library(plotrix)
+
+  ################################################################
+
+  #Set working directory
+
+  setwd("C:/Users/Kevin Le/PycharmProjects/Pig Data Black Box")
+
+  #load data after data treatment
+
+  load("Data/JRPData.Rdata")
+
+
+
+  #===============================================================
+  # DATA PREPARATION
+  #===============================================================
+
+  #Create a new dataframe which stores Data after treatment of missing data
+  No.NA.Data.0 <- data.frame(AnimalID=factor(),
+                             AGE=double(),
+                             DFI=double(),
+                             CFI=double(),
+                             stringsAsFactors=FALSE)
+
+  #----------------------------------------------------
+  # Attribute new easier name to each column
+  #----------------------------------------------------
+
+  #Order number of Animal_ID
+  ID <- unique(JRP_NA$ANIMAL_ID)
+
+  #Pig ID
+  Pig_ID <- JRP_NA$ANIMAL_ID
+
+  #Age (days)
+  Age <- JRP_NA$AGE
+
+  #Daily Feed Intake (kg)
+  DFI <- JRP_NA$FEED_INTAKE
+
+  #===============================================================
+  # For loop for automatically filtering missing data for all pigs
+  #===============================================================
+
+  # IDC = c(1:2,4:51, 53:79, 81:119) # animals 3, 52 and 80 are removed because of some problems
+
+  for(idc in seq_along(ID)){
+
+
+    # extract data for animal i
+
+    i <- ID[idc] # 90
+
+    #extract dataset for animal i
+    Data <- JRP_NA.0[JRP_NA.0$ANIMAL_ID == i,]
+    Data
+    #Age vector associated with
+    Age.plot <- Age[Pig_ID == i]
+
+    #DFI vector associated with animal i
+    DFI.plot <- DFI[Pig_ID == i]
+
+    # Plot DFI graph
+
+    # tiff(file = paste0("Graphs/Step0_graphs/DFI/", idc, ".", Data$ANIMAL_ID, ".", "DFI", ".png"), width = 6000, height = 3500, units = "px", res=600)
+    DFI_fig <- plot_ly(Data, x = Age.plot, y = DFI.plot, type = 'scatter', mode = 'markers') %>%
+      layout(title = paste("Daily feed intake measured from an auto-feeder","Pig ID:", ID[idc]), xaxis = list(title = 'Age, d'),
+         yaxis = list(title = 'Daily Feed Intake, kg/ d'))
+      geom_point(shape = 21, colour = "black", fill = "white", size = 4, stroke = 1.5)
+      scale_y_continuous(breaks=seq(0, 8, 1)) %>%
+      scale_x_continuous(breaks=seq(0, 300, 20)) %>%
+      theme(plot.title = element_text(hjust = .5)) %>%
+      theme(panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.background = element_blank(),
+            axis.line = element_line(colour = "black")
+      ) %>%
+      theme(axis.text=element_text(size=12),
+            axis.title=element_text(size=14,face="bold"))
+    png(filename = paste0("Graphs/Step0_graphs/Observasion_DFI/", idc, ".", ID[idc], ".", "DFI", ".png"),
+    height = 536 , width = 700, units = 'px', type="cairo-png")
+    par(mar=c(4.5,4.5,4.5,1.5))
+    plot(Age.plot, DFI.plot, # Observation data of DFI
+          main = paste0("Daily Feed Intake measured from automatic feeder", "\nPig ID = ", i, ",", " idc = ", idc),
+          ylab = "Daily Feed Intake (kg)",
+          xlab = "Age (days)",
+          type="p", col ="black", cex.main = 1.7, cex = 1.5,
+          cex.axis = 1.5, cex.lab = 1.5)
+    legend("topleft", "DFI (kg/ day)",
+           col="black",
+           pch=1, bty = "n", cex = 1.2)
+    saveWidget(DFI_fig, file=paste0("Graphs/Step0_graphs/Observasion_DFI/", idc, ".", ID[idc], ".", "DFI", ".html"))
+    dev.off()
+    #----------------------------------------------------
+    # Calculate CFI from DFI
+    #----------------------------------------------------
+
+    #CFI
+    CFI.plot <- NULL
+    CFI.plot[1] <- DFI.plot[1]
+    for(j in 2:length(Age.plot)){
+      CFI.plot[j] <- CFI.plot[j-1]+DFI.plot[j]
+    }
+
+    # Plot CFI graph
+    # tiff(file = paste0("Pig Data Black Box/Graphs/Step0_graphs/CFI/", idc, ".", Data$ANIMAL_ID, ".", "CFI", ".png"), width = 6000, height = 3500, units = "px", res=600)
+    CFI.Fig <- ggplot(Data, aes(x = Age.plot, y = CFI.plot)) +
+      geom_point(shape = 21, colour = "black", fill = "white", size = 3, stroke = 1.2) +
+      xlab("Age, d") +
+      ylab("Cumulative Feed Intake, kg") +
+      scale_y_continuous(breaks=seq(0, 400, 40)) +
+      scale_x_continuous(breaks=seq(0, 300, 20)) +
+      ggtitle(paste("Cumulative Feed Intake","\nPig ID:", ID[idc])) +
+      theme(plot.title = element_text(hjust = .5)) +
+      theme(panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.background = element_blank(),
+            axis.line = element_line(colour = "black")
+      ) +
+      theme(axis.text=element_text(size=12),
+            axis.title=element_text(size=14,face="bold"))
+    saveWidget(ggplotly(CFI.Fig), file=paste0("C:/Users/Kevin Le/PycharmProjects/Pig Data Black Box/Graphs/Step0_graphs/Observasion_CFI/", idc, ".", ID[idc], ".", "CFI", ".html"))
+    dev.off()
+
+
+  } # end of FOR loop
+
---
+++ Package/Step4_functions_X0_problem.R
@@ -0,0 +1,298 @@
+
+  #==========================================================================
+  # Function of perturbation
+  # Masoomeh Jaap Hieu
+  # 15/11/2017
+  #==========================================================================
+
+
+                        #============================ ======== ========
+                        # New function discussion Jaap and Hieu 14/11/2017
+                        #============================ ======== ========
+
+  #=======================================================================================
+  # 1. function for perturbation model  -- Model ode.pert.14/11 - Jaap modified 03/08/2018
+  #=======================================================================================
+
+  #------------------------------
+  # Linear function for TTC
+  #------------------------------
+
+  #Fitting initial parameters to the model
+  ODE.CFI.0 <- function(Time, State, Pars) {
+  #
+  p1 <- Pars[1]  #percentage of decrease of ITD during the 1st perturbation
+  max.compFI1 <- Pars[2]
+  tbeg1 <- Pars[3]
+  tstop1 <- Pars[4]
+  a <- Pars[5]
+  b <- Pars[6]
+  
+  
+  #
+  with(as.list(c(State, Pars)), {
+    # 
+    x <- Time
+    #
+    ITC.CFI <- a +b*x  #returns the cubic function
+    ITC.DFI <- b
+    #
+    onoff <- ifelse(Time>tbeg1 & Time<tstop1, 1, 0)
+    CompFI <- (1-CumFI/ITC.CFI)*max.compFI1
+    #
+    dCumFI <- (onoff*(p1-1) + CompFI + 1)*ITC.DFI
+    #
+    return(list(dCumFI))
+  })
+}
+
+  #Function to estimate parameters
+  ODE.CFI.optim.0 <- function(Time, State, Pars) {
+  #
+  p1 <- Pars[1]  #percentage of decrease of ITD during the 1st perturbation
+  max.compFI1 <- Pars[2]
+  tbeg1 <- Pars[3]
+  tstop1 <- Pars[4]
+  a <- Pars[5]
+  b <- Pars[6]
+  # CumFI <- Pars[7]
+  # c           = Pars[7]
+  # d           = Pars[8]
+  
+  #
+  with(as.list(c(State, Pars)), {
+    # 
+    x <- Time
+    #
+    ITC.CFI <- a + b*x  #returns the cubic function
+    ITC.DFI <- b
+    #
+    onoff <- ifelse(Time>tbeg1 & Time<tstop1, 1, 0)
+    CompFI <- (1-CumFI/ITC.CFI)*max.compFI1
+    #
+    dCumFI <- (onoff*(p1-1) + CompFI + 1)*ITC.DFI
+    #
+    return(list(dCumFI))
+  })
+}
+
+  #Objective function
+  ODE.CFI.obj.0 <- function(P, data){
+  data <- Data.xy
+  f <- ode(yinit, times, ODE.CFI.optim.0, P)
+  optim.f <- sqrt(sum((data$CFI.plot- f[, 2])^2))
+  return (optim.f)
+}
+
+<<<<<<< Updated upstream
+=======
+  #nls function for initial parameter
+  ODE.CFI.obj.nls.0 <- function(p1,p2){
+    P <- c(p1,p2)
+    data <- Data.xy
+    f <- ode(yinit, times, ODE.CFI.optim.0, P)
+    return ( f[,2])
+  }
+
+>>>>>>> Stashed changes
+  #------------------------------
+  # Quadratic function for TTC
+  #------------------------------
+
+  #Fitting initial parameters to the model
+  ODE.CFI.1 <- function(Time, State, Pars) {
+    #
+    p1 <- Pars[1]  #percentage of decrease of ITD during the 1st perturbation
+    max.compFI1 <- Pars[2]
+    tbeg1 <- Pars[3]
+    tstop1 <- Pars[4]
+    a <- Pars[5]
+    b <- Pars[6]
+    c <- Pars[7]
+
+    #
+    with(as.list(c(State, Pars)), {
+      #
+      x <- Time
+      #
+      ITC.CFI <- a + b*x + c*x^2   #returns the cubic function
+      ITC.DFI <- b+2*c*x
+      #
+      onoff <- ifelse(Time>tbeg1 & Time<tstop1, 1, 0)
+      CompFI <- (1-CumFI/ITC.CFI)*max.compFI1
+      #
+      dCumFI <- (onoff*(p1-1) + CompFI + 1)*ITC.DFI
+      #
+      return(list(c(dCumFI)))
+    })
+  }
+
+  #Function to estimate parameters
+  ODE.CFI.optim.1 <- function(Time, State, Pars) {
+    #
+    p1 <- Pars[1]  #percentage of decrease of ITD during the 1st perturbation
+    max.compFI1 <- Pars[2]
+    tbeg1 <- Pars[3]
+    tstop1 <- Pars[4]
+    # a           = Pars[5]
+    # b           = Pars[6]
+    # c           = Pars[7]
+    # d           = Pars[8]
+
+    #
+    with(as.list(c(State, Pars)), {
+      #
+      x <- Time
+      #
+      ITC.CFI <- a + b*x + c*x^2  #returns the cubic function
+      ITC.DFI <- b+2*c*x
+      #
+      onoff <- ifelse(Time>tbeg1 & Time<tstop1, 1, 0)
+      CompFI <- (1-CumFI/ITC.CFI)*max.compFI1
+      #
+      dCumFI <- (onoff*(p1-1) + CompFI +1)*ITC.DFI
+      #
+      return(list(dCumFI))
+    })
+  }
+
+  #Objective function
+  ODE.CFI.obj.1 <- function(P, data){
+    data <- Data.xy
+    f <- ode(yinit, times, ODE.CFI.optim.1, P)
+    optim.f <- sqrt(sum((data$CFI.plot- f[, 2])^2))
+    return (optim.f)
+  }
+
+  #----------------------------------
+  # Quadratic-linear function for TTC
+  #----------------------------------
+
+  #Fitting initial parameters to the model
+  ODE.CFI.2 <- function(Time, State, Pars) {
+    #
+    p1 <- Pars[1]  #percentage of decrease of ITD during the 1st perturbation
+    max.compFI1 <- Pars[2]
+    tbeg1 <- Pars[3]
+    tstop1 <- Pars[4]
+    a <- Pars[5]
+    b <- Pars[6]
+    c <- Pars[7]
+
+    #
+    with(as.list(c(State, Pars)), {
+    #
+    x <- Time
+    ind1 <- as.numeric(x < Xs)
+    #
+    ITC.CFI <- ind1*(a+b*x+c*x^2)+(1-ind1)*((a+b*(Xs)+c*(Xs)^2)+(b+2*c*(Xs))*(x-(Xs)))  #returns the cubic function
+    ITC.DFI <- ind1*(b+2*c*x) + (1 - ind1)*(b+2*c*(Xs))
+    #
+    onoff <- ifelse(Time>tbeg1 & Time<tstop1, 1, 0)
+    CompFI <- (1-CumFI/ITC.CFI)*max.compFI1
+    #
+    dCumFI <- (onoff*(p1-1) + CompFI + 1)*ITC.DFI
+    #
+    return(list(c(dCumFI)))
+    })
+  }
+
+  #Function to estimate parameters
+  ODE.CFI.optim.2 <- function(Time, State, Pars) {
+     #
+     p1 <- Pars[1]  #percentage of decrease of ITD during the 1st perturbation
+     max.compFI1 <- Pars[2]
+     tbeg1 <- Pars[3]
+     tstop1 <- Pars[4]
+     # a           = Pars[5]
+     # b           = Pars[6]
+     # c           = Pars[7]
+     # d           = Pars[8]
+
+     #
+     with(as.list(c(State, Pars)), {
+     #
+     x <- Time
+     ind1 <- as.numeric(x < Xs)
+     #
+     ITC.CFI <- ind1*(a+b*x+c*x^2)+(1-ind1)*((a+b*(Xs)+c*(Xs)^2)+(b+2*c*(Xs))*(x-(Xs)))  #returns the cubic function
+     ITC.DFI <- ind1*(b+2*c*x) + (1 - ind1)*(b+2*c*(Xs))
+     #
+     onoff <- ifelse(Time>tbeg1 & Time<tstop1, 1, 0)
+     CompFI <- (1-CumFI/ITC.CFI)*max.compFI1
+     #
+     dCumFI <- (onoff*(p1-1) + CompFI + 1)*ITC.DFI
+     #
+     return(list(dCumFI))
+     })
+   }
+
+  #  objective function
+  ODE.CFI.obj.2 <- function(P, data){
+     data <- Data.xy
+     f <- ode(yinit, times, ODE.CFI.optim.2, P)
+     optim.f <- sqrt(sum((data$CFI.plot- f[, 2])^2))
+     return (optim.f)
+   }
+
+  # ##############################################################################################################################
+  # # ESTIMATE ALL PARAMETERS (INCLUDING THE TTC)
+  # ##############################################################################################################################
+  #
+ ODE.CFI.obj.nls <- function(p1, p2, p3, p4){
+   P = c(p1, p2, p3, p4)
+   data = Data.xy
+   f <- ode(yinit, times, ODE.CFI.optim, P)
+   # optim.f = sqrt(sum((data$CFI.plot- f[,2])^2))
+   return ( f[,2])
+ }
+
+ ODE.CFI.obj.nls1 <- function(p4){
+   p2 = coef(st2)[2]
+   p4 = coef(st2)[4]
+   p3 = coef(st2)[3]
+   P = c(p1, p2, p3, p4)
+   data = Data.xy
+   f <- ode(yinit, times, ODE.CFI.optim, P)
+   # optim.f = sqrt(sum((data$CFI.plot- f[,2])^2))
+   return ( f[,2])
+ }
+
+ ODE.CFI.optim.all <- function(Time, State, Pars) {
+   #
+   p1          = Pars[1]  #percentage of decrease of ITD during the 1st perturbation
+   max.compFI1 = Pars[2]
+   tbeg1       = Pars[3]
+   tstop1      = Pars[4]
+   a           = Pars[5]
+   b           = Pars[6]
+   c           = Pars[7]
+   d           = Pars[8]
+
+   #
+   with(as.list(c(State, Pars)), {
+     #
+     ITC.CFI = a+b*Time+c*Time^2+d*Time^3
+     ITC.DFI = b+2*c*Time+3*d*Time^2
+     onoff = ifelse(Time>tbeg1 & Time<tstop1,1,0)
+     compFI = (1-CumFI/ITC.CFI)*max.compFI1
+
+     #
+     dCumFI    =   (onoff*(1-p1) + (1 - onoff)) *
+       ITC.DFI *(1+max.compFI1*(1 - CumFI/ITC.CFI))
+     #
+     return(list(c(dCumFI)))
+   })
+ }
+
+
+ ##-----------------------------
+ ##  objective function
+ ##-----------------------------
+
+ ODE.CFI.obj.all <- function(P, data){
+   data = Data.xy
+   f <- ode(yinit, times, ODE.CFI.optim.all, P)
+   optim.f = sqrt(sum((data$CFI.plot- f[,2])^2))
+   return (optim.f)
+ }
